aec47e37846e3f441ab377f325132f071099bf8b
asagafonov <a.s.agafonov@yandex.ru>
2020-03-05 22:10:24 +0300

    Added readline-sync library and the code now asks for user's name to play

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,3 +1,9 @@
 #!/usr/bin/env node
 
 console.log("Welcome to the Brain Games!");
+
+const readlineSync = require('readline-sync');
+const askForName = readlineSync.question('May I have your name? ');
+const greetUser = console.log(`Hello, ${askForName}!`)
+
+greetUser;
