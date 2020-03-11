2545171d189a250d236dcb31e03b1991dc1516a7
asagafonov <asagafonov@users.noreply.github.com>
2020-03-11 18:31:20 +0300

    Finalized brain-calc.js logics

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,4 +1,5 @@
 import readlineSync from 'readline-sync';
+import askForName from './ask-for-name.js'
 
 const generateRandomNumber = () => Math.floor(Math.random() * 100);
 
@@ -25,18 +26,21 @@ const generateEquation = () => {
   const firstOperand = generateRandomNumber();
   const secondOperand = generateRandomNumber();
   const operator = generateOperator();
-  return `${firstOperand} ${operator} ${secondOperand}`;
+  const equation = `${firstOperand} ${operator} ${secondOperand}`;
+  return equation;
 };
 
-const solveEquation = () => {
-  const equation = generateEquation();
-  const eqArray = equation.split(' ');
-  if (eqArray[1] === '+') {
-    return eqArray[0] + eqArray[2];
-  } if (eqArray[1] === '-') {
-    return eqArray[0] - eqArray[2];
-  } if (eqArray[1] === '*') {
-    return eqArray[0] * eqArray[2];
+const solveEquation = (string) => {
+  const eqArray = string.split(' ');
+  const operator = eqArray[1];
+  const firstOperand = Number(eqArray[0]);
+  const secondOperand = Number(eqArray[2]);
+  if (operator === '+') {
+    return firstOperand + secondOperand;
+  } if (operator === '-') {
+    return firstOperand - secondOperand;
+  } if (operator === '*') {
+    return firstOperand * secondOperand;
   }
 };
 
@@ -44,9 +48,18 @@ const brainCalc = () => {
   console.log('What is the result of the following expression?');
   let iteration = 0;
   while (iteration < 3) {
-    const equationFormula = generateEquation();
-
-
+  const equation = generateEquation();
+  const answer = readlineSync.question(`Calculate ${equation} --> `);
+  if (Number(answer) === solveEquation(equation)) {
+    iteration += 1;
+    console.log(`Your answer is ${answer} \nCorrect!`);
+    if (iteration === 3) {
+      console.log(`Congratulations, ${askForName}! You win!`);
+    }
+  } else {
+    console.log(`Your answer is ${answer} \n${answer} is the wrong answer ;(. Correct answer was ${solveEquation(equation)} \nLet's try it again, ${askForName}!`);
+    break;
+    }
   }
 };
 
