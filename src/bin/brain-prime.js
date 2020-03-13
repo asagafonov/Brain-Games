e6fe50435cf9d6c6ed23294995014171bcb73805
asagafonov <asagafonov@users.noreply.github.com>
2020-03-13 18:08:50 +0300

    Create brain-prime.js

---
+++ src/bin/brain-prime.js
@@ -0,0 +1,47 @@
+import readlineSync from 'readline-sync';
+import askForName from './ask-for-name.js';
+
+const generateRandomNumberInRange = (min, max) => {
+  const minValue = Math.ceil(min);
+  const maxValue = Math.floor(max);
+  return Math.floor(Math.random() * (maxValue - minValue)) + minValue;
+};
+
+const askWhetherIsPrime = () => {
+  const randomNumber = generateRandomNumberInRange(1, 100);
+  if (//если число — простое) {
+    if (readlineSync.keyInYN(`Is the number ${number} prime?`)) {
+      console.log('Your answer is "yes" \nCorrect!');
+      return true;
+    }
+    console.log(`Your answer is "no" \n"no" is the wrong answer ;(. Correct answer was "yes" \nLet's try it again, ${askForName}!`);
+    return false;
+  } else {
+    if (readlineSync.keyInYN(`Is the number ${number} prime?`)) {
+      console.log(`Your answer is "yes" \n"yes" is the wrong answer ;(. Correct answer was "no" \nLet's try it again, ${askForName}!`);
+      return false;
+    }
+    console.log('Your answer is "no" \nCorrect!');
+    return true;
+  }
+  return false;
+};
+
+
+const brainPrime = () => {
+  console.log('Answer "yes" if the given number is prime. Otherwise answer "no".');
+  let iteration = 0;
+  while (iteration < 3) {
+    const question = askWhetherIsPrime();
+    if (question === true) {
+      iteration += 1;
+      if (iteration === 3) {
+        console.log(`Congratulations, ${askForName}! You win!`);
+      }
+    } else if (question === false) {
+      break;
+    }
+  }
+};
+
+export default brainPrime;
