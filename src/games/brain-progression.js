e1f297042f2788b5ab1bb4922c7f849eb942adae
asagafonov <a.s.agafonov@yandex.ru>
2020-03-30 13:45:55 +0300

    upd brain-progression.js

--- src/games/brain-progression.js
+++ src/games/brain-progression.js
@@ -1,10 +1,8 @@
 import generateRandomNumberInRange from '../utils/generate-random-number.js';
 import { runEngine } from '../index.js';
 
-const generateProgressionOfTen = () => {
+const generateProgressionOfTen = (starter, difference) => {
   const array = [];
-  const starter = generateRandomNumberInRange(1, 100);
-  const difference = generateRandomNumberInRange(1, 11);
   const maximum = starter + (difference * 10);
   for (let i = starter; i < maximum; i += difference) {
     array.push(i);
@@ -23,7 +21,9 @@ const createArray = (limit) => {
   const array = [];
   let iteration = 0;
   while (iteration < limit) {
-    const progression = generateProgressionOfTen();
+    const progressionStarter = generateRandomNumberInRange(1, 100);
+    const progressionDifference = generateRandomNumberInRange(1, 11);
+    const progression = generateProgressionOfTen(progressionStarter, progressionDifference);
     const hiddenProgression = hideDigit(progression);
     const hiddenProgressionToString = hiddenProgression.join(' ');
     const correctAnswer = String(progression[hiddenProgression.indexOf('..')]);
