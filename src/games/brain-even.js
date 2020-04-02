ed447d71ea19517ae07b483e1aedd59f9d8b97d3
asagafonov <a.s.agafonov@yandex.ru>
2020-04-02 23:04:40 +0300

    fix linter issues

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -3,13 +3,9 @@ import runEngine from '../index.js';
 
 const randomNumber = () => generateRandomNumberInRange(1, 100);
 
-const isEven = (num) => {
-  return num % 2 === 0;
-};
+const isEven = (num) => num % 2 === 0;
 
-const returnEvenOrNot = (num) => {
-  return isEven(num) ? 'yes' : 'no';
-};
+const returnEvenOrNot = (num) => isEven(num) ? 'yes' : 'no';
 
 const createArray = (limit) => {
   const array = [];
